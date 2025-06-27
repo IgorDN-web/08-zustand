e92833a4eb103a5c9c939824750e0d0448bdb912
IgorD <avtosklokyiv@gmail.com>
2025-06-27 21:07:49 +0300

    action

---
+++ app/notes/action/page.tsx
@@ -0,0 +1,10 @@
+// app/notes/action/page.tsx
+
+export default function PlaceholderPage() {
+    return (
+      <main style={{ padding: '2rem' }}>
+        <h1>This route is a placeholder. Redirecting...</h1>
+      </main>
+    );
+  }
+  
